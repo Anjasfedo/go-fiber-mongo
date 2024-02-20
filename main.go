73115308029bf6696b70f65bc7dd006a67d26c36
Anjasfedo <fedoaf98@gmail.com>
2024-02-20 23:06:12 +0700

    Define Database & Model

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
-import (
+import ()
 
-)
+type MongoInstance struct {
+	Client
+	Db
+}
+
+var mg MongoInstance
+
+const DB_NAME = "fiber-mongo"
+const MONGO_URL = "mongodb://localhost:27017" + DB_NAME
+
+type Employee struct {
+	Id     int64   `json:"id"`
+	Name   string  `json:"name"`
+	Age    int64   `json:"age"`
+	Salary float64 `json:"salary"`
+}
 
 func main() {
-	
+
 }
