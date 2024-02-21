880c6681ac53b6315a4d48627297a458ecaa4ed3
Anjasfedo <fedoaf98@gmail.com>
2024-02-21 23:55:24 +0700

    Get All Handler

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ const DB_NAME = "fiber-mongo"
 const MONGO_URI = "mongodb://localhost:27017" + DB_NAME
 
 type Employee struct {
-	Id     int64   `json:"id"`
+	Id     int64   `json:"id,omitempty" bson:"_id, omitempty"`
 	Name   string  `json:"name"`
 	Age    int64   `json:"age"`
 	Salary float64 `json:"salary"`
@@ -61,7 +61,20 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/employee", func(c *fiber.Ctx) error {
+		query := bson.D{{}}
 
+		cursor, err := mg.Db.Collection("employees").Find(c.Context(), query)
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+
+		var employees []Employee = make([]Employee, 0)
+
+		if err := cursor.All(c.Context(), &employees); err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+
+		return c.JSON(employees)
 	})
 
 	app.Post("/employee")
